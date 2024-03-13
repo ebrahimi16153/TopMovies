76f0d2d1f996e6baaa69ead57b1c5c501ab9e77d
Mohammad <ebrahimi2723@yahoo.com>
2024-03-13 22:40:24 +0330

    1 -> added a Gradient Component 2 -> start to implement Ui Home

--- app/src/main/java/com/github/ebrahimi16153/topmovies/ui/screens/Home.kt
+++ app/src/main/java/com/github/ebrahimi16153/topmovies/ui/screens/Home.kt
@@ -3,6 +3,7 @@ package com.github.ebrahimi16153.topmovies.ui.screens
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +11,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.github.ebrahimi16153.topmovies.models.ResponseOfMainBannerMovie
 import com.github.ebrahimi16153.topmovies.viewModel.HomeViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -35,29 +38,21 @@ fun Home(navHostController: NavHostController, homeViewModel: HomeViewModel) {
     }
     homeViewModel.getMainBannerMovieList(1)
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize()) {
 
-//        LaunchedEffect(key1 = true) {
-//            loading.value = homeViewModel.mainBannerError.value
-//        }
-//
-        LaunchedEffect(key1 = true) {
-            homeViewModel.getMainBannerMovieList(1)
+
+
         }
-        homeViewModel.getMainBannerMovieList(1)
-
-        val mainBannerMovieList = homeViewModel.mainBannerMovieList.collectAsState()
-
-
-        LazyColumn {
-            items(items = mainBannerMovieList.value){
-                Text(text = it.title!!)
-            }
-        }
-
     }
 
 
+}
+
+
+
+@Composable
+fun MainBanner(list: List<ResponseOfMainBannerMovie.Data>){
 
 
 
--- app/src/main/java/com/github/ebrahimi16153/topmovies/util/Component.kt
+++ app/src/main/java/com/github/ebrahimi16153/topmovies/util/Component.kt
@@ -1,8 +1,19 @@
 package com.github.ebrahimi16153.topmovies.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun MainBanner(){
+fun gradient(isVerticalGradient:Boolean,colors:List<Color>): Brush {
+
+    val start =
+        if (isVerticalGradient)
+            Offset(0f,Float.POSITIVE_INFINITY)
+        else
+            Offset(Float.POSITIVE_INFINITY,0f)
+
+return Brush.linearGradient(colors = colors , start = start )
 
 }
